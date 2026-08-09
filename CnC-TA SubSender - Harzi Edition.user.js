fc75b22981887773e1c77dc760e148f895fbff38
Harzi66 <harzinger@t-online.de>
2026-08-09 22:14:33 +0200

    Add CnC-TA SubSender - Harzi Edition user script

---
+++ CnC-TA SubSender - Harzi Edition.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name           CnC-TA SubSender - Harzi Edition
+// @namespace      https://github.com/Harzi66/CnC-TA-SubSender-Harzi-Edition
+// @version        0.1.8
+// @description    C&C TA SubSender - Harzi Edition
+// @author         Harzi
+// @match          https://*.alliances.commandandconquer.com/*/index.aspx*
+// @require        https://raw.githubusercontent.com/Harzi66/CnC-TA-SubSender-Harzi-Edition/main/CnC-TA%20SubSender%20-%20Harzi%20Edition.user.js
+// @grant          none
+// ==/UserScript==
