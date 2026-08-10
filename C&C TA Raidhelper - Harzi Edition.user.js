bdf1076899fb538b99530403620db96d6cf6ec9e
Harzi66 <harzinger@t-online.de>
2026-08-10 13:58:16 +0200

    Add CnC-TA Raidhelper - Harzi Edition user script
    Initial creation of the CnC-TA Raidhelper user script with metadata.

---
+++ C&C TA Raidhelper - Harzi Edition.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name           CnC-TA Raidhelper - HE
+// @namespace      https://github.com/Harzi66/CnC-TA-Harzi-Edition
+// @version        0.3.0
+// @description    Raidhelper mit frei einstellbarer Lower Range und Anzeige der Zielhöhe.
+// @author         Harzi
+// @match          https://*.alliances.commandandconquer.com/*/index.aspx*
+// @require        https://raw.githubusercontent.com/Harzi66/CnC-TA-Raidhelper-HE/main/C%26C%20TA%20Raidhelper%20-%20HE-0.3.0.user.js
+// @grant          none
+// ==/UserScript==
