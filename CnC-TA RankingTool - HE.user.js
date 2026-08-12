334fac07664bc899529364256adea5d95feca943
Harzi66 <harzinger@t-online.de>
2026-08-12 15:52:07 +0200

    Add CnC-TA RankingTool - HE user script
    Initial creation of the CnC-TA RankingTool - HE user script with metadata.

---
+++ CnC-TA RankingTool - HE.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name           CnC-TA RankingTool - HE
+// @namespace      https://github.com/Harzi66/CnC-TA-RankingTool-HE
+// @version        1.3.0
+// @description    Spieler- und Allianz-Ranking mit Rangbereich, Punktetendenzen und Spielersuche
+// @author         Harzi66
+// @match          https://*.alliances.commandandconquer.com/*/index.aspx*
+// @require        https://raw.githubusercontent.com/Harzi66/CnC-TA-RankingTool-HE/main/CnC-TA%20RankingTool%20-%20HE-1.3.0.user.js
+// @grant          none
+// ==/UserScript==
