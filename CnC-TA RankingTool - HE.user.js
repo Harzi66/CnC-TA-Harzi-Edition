c624db0de8fea5e79d4849b53f268f8f5918eecd
Harzi66 <harzinger@t-online.de>
2026-08-13 15:06:08 +0200

    Update version and require URL in userscript
    Updated the version and require URL for the userscript.

--- CnC-TA RankingTool - HE.user.js
+++ CnC-TA RankingTool - HE.user.js
@@ -1,10 +1,9 @@
 // ==UserScript==
 // @name           CnC-TA RankingTool - HE
 // @namespace      https://github.com/Harzi66/CnC-TA-RankingTool-HE
-// @version        1.5.14
 // @description    Spieler- und Allianz-Ranking mit Rangbereich, Punktetendenzen und Spielersuche
 // @author         Harzi66
 // @match          https://*.alliances.commandandconquer.com/*/index.aspx*
-// @require        https://raw.githubusercontent.com/Harzi66/CnC-TA-RankingTool-HE/main/CnC-TA-RankingTool-HE-1.5.14.user.js
+// @require https://raw.githubusercontent.com/Harzi66/CnC-TA-RankingTool-HE/main/CnC-TA-RankingTool-HE.user.js
 // @grant          none
 // ==/UserScript==
