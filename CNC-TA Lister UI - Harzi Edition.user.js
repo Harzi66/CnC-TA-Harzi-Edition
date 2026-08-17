2277c3f1116e46ba1d78f895f7a3b0b5b2cbf8e9
Harzi66 <harzinger@t-online.de>
2026-08-17 15:00:55 +0200

    Update version and require URL in user script

--- CNC-TA Lister UI - Harzi Edition.user.js
+++ CNC-TA Lister UI - Harzi Edition.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name           CnC-TA Lister UI - HE
 // @namespace      https://github.com/Harzi66/CnC-TA-Lister-UI-Harzi-Edition
-// @version        1.25.0-Beta1
+// @version        1.32.0-dev4
 // @description    Erweiterte Version des CnC-TA Lister UI
 // @author         Harzi66
 // @match          https://*.alliances.commandandconquer.com/*/index.aspx*
-// @require        https://raw.githubusercontent.com/Harzi66/CnC-TA-Lister-UI-Harzi-Edition/main/CnC-TA_Lister_UI.user.js
+// @require        https://raw.githubusercontent.com/Harzi66/CnC-TA-Lister-UI-Harzi-Edition/main/CnC-TA%20Lister%20UI%20-%20Harzi%20Edition.user.js
 // @grant          none
 // ==/UserScript==
