5f9c8fe40b87a6c17aad0cfbd1284c60aec4ed8b
Harzi66 <harzinger@t-online.de>
2026-08-20 06:41:49 +0200

    Fix formatting of require statement in user script

--- CnC-TA RankingTool - HE.user.js
+++ CnC-TA RankingTool - HE.user.js
@@ -4,6 +4,6 @@
 // @description    Spieler- und Allianz-Ranking mit Rangbereich, Punktetendenzen und Spielersuche
 // @author         Harzi66
 // @match          https://*.alliances.commandandconquer.com/*/index.aspx*
-// @require https://raw.githubusercontent.com/Harzi66/CnC-TA-RankingTool-HE/main/CnC-TA-RankingTool-HE.user.js
+// @require        https://raw.githubusercontent.com/Harzi66/CnC-TA-RankingTool-HE/main/CnC-TA-RankingTool-HE.user.js
 // @grant          none
 // ==/UserScript==
